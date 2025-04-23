0edc9ec75c8881fd298af8ee9a97274d094d14d6
EXOGAMER007 <daffa8110@gmail.com>
2025-04-23 17:12:48 +0800

    commit pertama

---
+++ TUGAS 1/Prototype/Catatan.vb
@@ -0,0 +1,21 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Catatan
+    Private Sub BtnKembali_Click(sender As Object, e As EventArgs) Handles BtnKembali.Click
+
+        Dim ubah As String = "update tbnote set Note = (@note) where id = @id"
+        CMD = New MySqlCommand(ubah, CONN)
+        CMD.Parameters.AddWithValue("@id", "1")
+        CMD.Parameters.AddWithValue("@note", TxtCatatan.Text)
+        CMD.ExecuteNonQuery()
+        Me.Close()
+    End Sub
+
+    Private Sub Catatan_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CMD = New MySqlCommand("select Note from tbnote where id = @id", CONN)
+        CMD.Parameters.AddWithValue("@id", "1")
+        TxtCatatan.Text = ""
+        Dim result As Object = CMD.ExecuteScalar()
+        TxtCatatan.Text = result
+    End Sub
+End Class
